3a3b495c77646de26c855feee55e24e34dcca38b
Suna <xkq3355@naver.com>
2022-12-28 00:07:39 +0900

    12-28 집에서

--- 2일차/fs.js
+++ 2일차/fs.js
@@ -3,8 +3,8 @@
 */
 const fs = require("fs");
 fs.readFile("./template/test.txt","utf-8",(err,data)=>{
-  console.log(data);
+  console.log(data); // 나중에 이게 실행됨
 });
-console.log("the end");
+console.log("the end"); // 이게 먼저 실행됨
 
 
--- 2일차/server.js
+++ 2일차/server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req,res)=>{ // requestListener : 요청이 들
 
   if(myurl.pathname.startsWith("/page")){ // startsWith("특정문자열") : 해당문자열이 특정문자열로 시작하는가
     const pagename = "./template" + myurl.pathname.substring(5) + ".html"; // /page 까지 자르고 그뒤에 파일명
-    fs.readFile(pagename,"utf8",(err,data)=>{ // (파일경로, 인코딩, 콜백함수);
+    fs.readFile(pagename,"utf8",(err,data)=>{ // 비동기 (파일경로, 인코딩, 콜백함수);
                                               // 파일을 다읽고나면 콜백함수가 실행됨 date : 파일을 read한 내용
       res.end(data);
     });
@@ -21,4 +21,4 @@ const server = http.createServer((req,res)=>{ // requestListener : 요청이 들
 
 server.listen(3000, () => { // listeningListener : 서버가 동작(구동)되면 호출하는 함수
   console.log("server running http://localhost:3000"); // localhost : 127.0.0.1 과 같음
-}); 
+});
