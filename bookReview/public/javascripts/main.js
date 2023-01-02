73afcce92e805a38cea8e2367a7666f1afbb2cfa
Suna <xkq3355@naver.com>
2023-01-03 00:18:02 +0900

    2023-1-3 집에서_공공데이터api 파싱

---
+++ bookReview/public/javascripts/main.js
@@ -0,0 +1,55 @@
+ /* main.js */
+let books = [];
+
+//공공데이터 포털 API : XML형식
+const url = 'http://api.kcisa.kr/openapi/service/rest/meta13/getNLSF0401?serviceKey=d2252ebd-bfd4-44d8-b005-ff9a8878b9a8&numOfRows=10&pageNo=1'; /*URL*/
+fetch(url) // get방식 조회
+ .then(res => res.text()) //text로 가져옴
+ .then(res => {
+  let parser = new DOMParser();
+  let xmlDoc = parser.parseFromString(res, "application/xml"); //text를 xml형식으로 변환
+  let count = xmlDoc.getElementsByTagName("item"); // 갯수
+  let img = xmlDoc.getElementsByTagName("referenceIdentifier"); // 이미지
+  let title = xmlDoc.getElementsByTagName("title"); // 책이름
+  let write = xmlDoc.getElementsByTagName("rights"); // 저자
+  let description = xmlDoc.getElementsByTagName("description"); // 줄거리
+
+  for(let i=0; i<count.length; i++){
+    books.push({
+                img: img[i].innerHTML, 
+                title: title[i].innerHTML, 
+                write: write[i].innerHTML, 
+                description : description[i].innerHTML
+              });
+  }
+
+  for(let i = 0 ; i<books.length-1; i++){ //10개있지만 미관상 9개만
+    popular_div.innerHTML += popular_html(books[i]);
+  }
+ })
+
+ function popular_html(books){
+  let html = `<div class="col mb-5 div_piece">
+                <div class="card h-100">
+                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">추천</div>
+                    <!-- Product image-->
+                    <img class="card-img-top" src="${books.img}" alt="도서사진" />
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder">${books.title}</h5>
+                            <!-- Product reviews-->
+                            <div class="mb-2">- ${books.write}</div>
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">자세히보기</a></div>
+                    </div>
+                </div>
+              </div>`
+  return html;
+}
+
+
---
+++ bookReview/public/javascripts/signUp.js
@@ -0,0 +1,32 @@
+const url = "/members";
+insert_Member(); // 회원가입
+
+function insert_Member(){
+  btnSignUp.addEventListener("click",function(){
+    if(sign_userPw.value == sign_userPw_check.value){ // 비밀번호가 같을때 실행
+      
+      let data = {
+                  userId: sign_userId.value,
+                  password: sign_userPw.value,
+                  nickName: sign_userNickName.value,
+                  phone: sign_userPhone.value,
+                  email: sign_userEmail.value,
+                  address: sign_userAddress.value
+                  }
+
+      fetch(url, { 
+        method : "POST",
+        headers : {
+                  "Content-type":"application/json"
+                  }, 
+        body:JSON.stringify(data)}) // json형태(string)여야함
+        .then(()=>{
+          alert("회원가입이 되었습니다");
+          location.assign("./main.html");
+        })
+     }else{ // 비밀번호가 같지않다면
+      alert("비밀번호가 같지 않습니다. 다시 입력해주세요");
+      sign_userPw.focus(); // 비밀번호 입력하는 부분에 포커스
+     }
+  });
+}
